c37f0cabf76a7da5fbc6adf5b7a3106cf6e87daa
João Targino <joaoetargino@gmail.com>
2024-08-02 16:18:57 -0300

    feat: created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export function Header() {
+  
+  return (
+    <>
+      <header className="drop-shadow-lg sticky top-0 z-50 flex px-[5%]  w-full justify-center py-4 bg-slate-800 md gap-4 lg:flex-col items-center text-slate-100 font-sans">
+        <img src="../../public/logo.png" alt="" className={"h-16"} />
+      </header>
+    </>
+  )
+}
